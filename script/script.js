634d5e7fd61fad666b1daabfe08a5753adc0141e
Simon Barras <simon.barras02@gmail.com>
2022-03-11 09:14:43 +0100

    About filled

--- script/script.js
+++ script/script.js
@@ -9,69 +9,69 @@ const computerScoreElem = document.getElementById("computer-score");
 
 let lastTime;
 let play = true;
+
 function update(time) {
-  if (lastTime != null) {
-    const delta = time - lastTime;
-    if (play) {
-      ball.update(delta, [playerPaddle.rect(), computerPaddle.rect()]);
-      computerPaddle.update(delta, ball.y);
+    if (lastTime != null) {
+        const delta = time - lastTime;
+        if (play) {
+            ball.update(delta, [playerPaddle.rect(), computerPaddle.rect()]);
+            computerPaddle.update(delta, ball.y);
+        }
+        const hue = parseFloat(
+            getComputedStyle(document.documentElement).getPropertyValue("--hue")
+        );
+
+        document.documentElement.style.setProperty("--hue", hue + delta * 0.01);
+
+        if (isLose()) handleLose();
     }
-    const hue = parseFloat(
-      getComputedStyle(document.documentElement).getPropertyValue("--hue")
-    );
 
-    document.documentElement.style.setProperty("--hue", hue + delta * 0.01);
-
-    if (isLose()) handleLose();
-  }
-
-  lastTime = time;
-  window.requestAnimationFrame(update);
+    lastTime = time;
+    window.requestAnimationFrame(update);
 }
 
 function isLose() {
-  const rect = ball.rect();
-  return rect.right >= window.innerWidth || rect.left <= 0;
+    const rect = ball.rect();
+    return rect.right >= window.innerWidth || rect.left <= 0;
 }
 
 function handleLose() {
-  const rect = ball.rect();
-  if (rect.right >= window.innerWidth) {
-    playerScoreElem.textContent = parseInt(playerScoreElem.textContent) + 1;
-  } else {
-    computerScoreElem.textContent = parseInt(computerScoreElem.textContent) + 1;
-  }
-  ball.reset();
-  computerPaddle.reset();
+    const rect = ball.rect();
+    if (rect.right >= window.innerWidth) {
+        playerScoreElem.textContent = parseInt(playerScoreElem.textContent) + 1;
+    } else {
+        computerScoreElem.textContent = parseInt(computerScoreElem.textContent) + 1;
+    }
+    ball.reset();
+    computerPaddle.reset();
 }
 
 document.addEventListener("mousemove", (e) => {
-  playerPaddle.position = (e.y / window.innerHeight) * 100;
+    playerPaddle.position = (e.y / window.innerHeight) * 100;
 });
 
 document.addEventListener("scroll", (e) => {
-  play = window.scrollY <= 2;
+    play = window.scrollY <= 2;
 });
 
 window.requestAnimationFrame(update);
 
 // add class navbarDark on navbar scroll
 const header = document.querySelector(".navbar");
-console.log(header);
-window.onscroll = function () {
-  const top = window.scrollY;
-  if (top >= 100) {
-    header.classList.add("navbarDark");
-  } else {
-    header.classList.remove("navbarDark");
-  }
+window.onscroll = function() {
+    const top = window.scrollY;
+    if (top >= 100) {
+        header.classList.add("navbarDark");
+    } else {
+        header.classList.remove("navbarDark");
+    }
 };
 // collapse navbar after click on small devices
 const navLinks = document.querySelectorAll(".nav-item");
 const menuToggle = document.getElementById("navbarSupportedContent");
 
 navLinks.forEach((l) => {
-  l.addEventListener("click", () => {
-    new bootstrap.Collapse(menuToggle).toggle();
-  });
+    l.addEventListener("click", () => {
+        new bootstrap.Collapse(menuToggle).toggle();
+    });
 });
